972a892626f4b039c1f5a35738a9d23a42a9f4ac
chauhan-abhi <chauhanabhi1245@gmail.com>
2018-09-23 20:02:58 +0530

    register partials and using hbs helpers to make dyanamic content

--- server.js
+++ server.js
@@ -3,25 +3,41 @@ const hbs = require('hbs')
 
 var app = express()
 
+hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs')
 
 app.use(express.static(__dirname + '/public')) // __dirname- path to your web server
 
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear()
+})
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase()
+})
+
+// app.get('/', (req, res) => {
+//     //res.send('<h1>Hello Express</h1> ')
+//     res.send( {
+//         name: 'Abhi',
+//         likes: [  
+//             'Fifa',
+//             'Gaming '
+//         ]
+//     })
+// })
+
 app.get('/', (req, res) => {
-    //res.send('<h1>Hello Express</h1> ')
-    res.send( {
-        name: 'Abhi',
-        likes: [  
-            'Fifa',
-            'Gaming '
-        ]
+    res.render('home.hbs', {
+        pageTitle: 'Home page',
+        welcomeMessage: 'Welcome here',
     })
+    
 })
 
 app.get('/about', (req, res) => {
     res.render('about.hbs', {
         pageTitle: 'About Page',
-        currentYear: new Date().getFullYear()
     })
 })
 
@@ -33,6 +49,6 @@ app.get('/bad', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server is up on 3000 ')
+app.listen(8000, () => {
+    console.log('Server is up on 8000 ')
 })
